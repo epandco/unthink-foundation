b022668a71d2947a186c59c831388403cb5e274e
Tommy Philyaw <tommy.philyaw@epandcompany.com>
2020-04-19 22:08:59 -0400

    Print route table

--- src/foundation/unthink-generator.ts
+++ src/foundation/unthink-generator.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import { ResourceDefinition } from './resource-definition';
 
 export interface UnthinkViewRenderer {
@@ -19,6 +20,28 @@ export class UnthinkGenerator<ResourceMiddleware> {
   add(resourceDefinition: ResourceDefinition<ResourceMiddleware>): UnthinkGenerator<ResourceMiddleware> {
     this.resourceDefinitions.push(resourceDefinition);
     return this;
+  }
+
+  printRouteTable(): void {
+    const tab = '    ';
+
+    console.log('Route Table:');
+
+    for (const resd of this.resourceDefinitions) {
+      const basePath = resd.basePath ?? '/';
+      console.log(`${tab}Resource: ${resd.name} - ${basePath}`);
+
+      for (const rd of resd.routes) {
+        const prefix = rd.prefix ?? '';
+
+        for (const m in rd.methods) {
+          const fullPath = path.join(prefix, basePath, rd.path);
+          console.log(`${tab}${tab}${m.toUpperCase()} ${rd.__routeType} ${fullPath}`);
+        }
+      }
+    }
+
+    console.log();
   }
 
   generate(): void {
